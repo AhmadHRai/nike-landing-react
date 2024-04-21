5fe097ccf567a55522ab5ce521ec909f40ce4a28
Ahmad Alrai <ahmadhalraie@gmail.com>
2024-04-21 12:21:36 +0300

    fix mobile nav z-index

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,34 +6,34 @@ import { navLinks } from '../constants';
 const Nav = () => {
   const [showNavbar, toggleNavbar] = useState(false);
   return (
-    <header className="padding-x py-8 absolute z-10 w-full">
-      <nav className="flex justify-between items-center max-container">
-        <a href="/">
+    <header className='padding-x py-8 absolute z-10 w-full'>
+      <nav className='flex justify-between items-center max-container'>
+        <a href='/'>
           <img
             src={headerLogo}
-            alt="logo"
+            alt='logo'
             width={129}
             height={29}
-            className="m-0 w-[129px] h-[29px]"
+            className='m-0 w-[129px] h-[29px]'
           />
         </a>
-        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+        <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
           {navLinks.map((item) => (
             <li key={item.label}>
               <a
                 href={item.href}
-                className="font-montserrat leading-normal text-lg text-slate-gray hover:bg-coral-red hover:text-white hover:rounded-xl p-3"
+                className='font-montserrat leading-normal text-lg text-slate-gray hover:bg-coral-red hover:text-white hover:rounded-xl p-3'
               >
                 {item.label}
               </a>
             </li>
           ))}
         </ul>
-        <div className="hidden max-lg:block cursor-pointer">
+        <div className='hidden max-lg:block cursor-pointer'>
           {!showNavbar ? (
             <img
               src={hamburger}
-              alt="hamburger icon"
+              alt='hamburger icon'
               width={25}
               height={25}
               onClick={() => {
@@ -41,23 +41,23 @@ const Nav = () => {
               }}
             />
           ) : (
-            <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-blue-100 drop-shadow-xl">
+            <div className='fixed right-0 bottom-0 z-50 h-full w-[300px] bg-blue-100 drop-shadow-xl'>
               <div
-                className="flex justify-end py-10 px-14"
+                className='flex justify-end py-10 px-14'
                 onClick={() => {
                   toggleNavbar(!showNavbar);
                 }}
               >
                 <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="w-6 h-6"
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 24 24'
+                  fill='currentColor'
+                  className='w-6 h-6'
                 >
                   <path
-                    fillRule="evenodd"
-                    d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z"
-                    clipRule="evenodd"
+                    fillRule='evenodd'
+                    d='M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z'
+                    clipRule='evenodd'
                   />
                 </svg>
               </div>
@@ -65,11 +65,11 @@ const Nav = () => {
                 {navLinks.map((item) => (
                   <li
                     key={item.label}
-                    className=" p-4 list-none hover:bg-blue-200 w-full rounded-lg"
+                    className=' p-4 list-none hover:bg-blue-200 w-full rounded-lg'
                   >
                     <a
                       href={item.href}
-                      className="font-montserrat font-medium leading-normal text-lg text-gray-600"
+                      className='font-montserrat font-medium leading-normal text-lg text-gray-600'
                     >
                       {item.label}
                     </a>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -17,9 +17,7 @@ const Hero = () => {
         <p className='text-xl font-montserrat text-coral-red'>Our Summer collections</p>
 
         <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold'>
-          <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>
-            The New Arrival
-          </span>
+          <span className='xl:bg-white xl:whitespace-nowrap relative pr-10'>The New Arrival</span>
           <br />
           <span className='text-coral-red inline-block mt-3'>Nike</span> Shoes
         </h1>
@@ -45,7 +43,7 @@ const Hero = () => {
           alt='shoe colletion'
           width={610}
           height={502}
-          className='object-contain relative z-10'
+          className='object-contain relative'
         />
 
         <div className='flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6'>
